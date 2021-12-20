fa083cd40f7ebb75597787bd5cb4cd14ed0210f3
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-20 09:55:28 +0300

    added login and logout urls

--- jiraniApp/urls.py
+++ jiraniApp/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path("accounts/register/",RegistrationView.as_view(success_url="/profile/"),name="django_registration_register",),
+    path("accounts/", include("django_registration.backends.one_step.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path('logout',auth_views.LogoutView.as_view(template_name = 'registration/logout.html'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html'),name='logout'),
+    url(r"^", include("jirani.urls")),
+
 ]
