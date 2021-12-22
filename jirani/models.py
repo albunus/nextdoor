e4f5388df2e2eec7c8a87a8d62240a9934a3e765
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-22 09:27:18 +0300

    added category class

--- jirani/models.py
+++ jirani/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime as dt
+from django.contrib.auth.models import User
+
+# cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
