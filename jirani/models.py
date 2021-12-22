a162e6d8bdf452cccb74f1939f976b8a2656d921
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-22 09:43:26 +0300

    added class Profile

--- jirani/models.py
+++ jirani/models.py
@@ -52,4 +52,19 @@ class Nextdoor(models.Model):
         return hood
 
     def __str__(self):
-        return self.name    
+        return self.name
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Nextdoor, on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def save_profile(self):
+        self.save()
+
+    def __str__(self):
+        return self.name        
