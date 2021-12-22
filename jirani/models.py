8767266f10e9e5ee04db2e98f358e11173584327
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-22 09:39:38 +0300

    added class nextdoor

--- jirani/models.py
+++ jirani/models.py
@@ -27,3 +27,29 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Nextdoor(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def create_nextdoor(self):
+        self.save()
+    @classmethod
+    def delete_nextdoor(cls,id):
+        cls.objects.filter(id=id).delete()
+    @classmethod
+    def update_nextdoor(cls,id):
+        cls.objects.filter(id=id).update()
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+    @classmethod
+    def find_nextdoor(cls,id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name    
