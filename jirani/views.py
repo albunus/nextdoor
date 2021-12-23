6949ed41ef38a965b29c53e90c070983f1ca56b5
albunus <albunus.nyalita@student.moringaschool.com>
2021-12-23 08:32:45 +0300

    added update profile function

--- jirani/views.py
+++ jirani/views.py
@@ -37,3 +37,54 @@ def profile(request):
     return render(request, 'profile.html', {'profile': profile, 'posts': posts, 'locations': locations, 'neighbourhood': neighbourhood, 'categories': category, 'businesses': businesses, 'contacts': contacts})
 
 
+@login_required(login_url="/accounts/login/")
+def update_profile(request):
+    if request.method == "POST":
+        current_user = request.user
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        name = request.POST["first_name"] + " " + request.POST["last_name"]
+        neighbourhood = request.POST["neighbourhood"]
+        location = request.POST["location"]
+        if location == "":
+            location = None
+        else:
+            location = Location.objects.get(name=location)
+        if neighbourhood == "":
+            neighbourhood = None
+        else:
+            neighbourhood = Nextdoor.objects.get(name=neighbourhood)
+            profile_image = request.FILES["profile_pic"]
+            profile_image = cloudinary.uploader.upload(profile_image)
+            profile_url = profile_image["url"]
+            user = User.objects.get(id=current_user.id)
+        if Profile.objects.filter(user_id=current_user.id).exists():
+            profile = Profile.objects.get(user_id=current_user.id)
+            profile.profile_pic = profile_url
+            profile.neighbourhood = neighbourhood
+            profile.location = location
+            profile.save()
+        else:
+            profile = Profile(user_id=current_user.id,
+                name=name,
+                profile_pic=profile_url,
+                neighbourhood=neighbourhood,
+                location=location,
+            )
+            profile.save_profile()
+            user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+
+        user.save()
+        return redirect("/profile", {"success": "Profile Updated Successfully"})
+    else:
+        return render(request, "profile.html", {"danger": "Profile Update Failed"})
+
+
+
+
+         
